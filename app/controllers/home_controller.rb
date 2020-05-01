e34240ec76743f190867c4aac671e734382bf7ef
stephannv <stephann@protonmail.com>
2020-05-01 14:06:36 -0300

    Add explore page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @latest_custom_designs = CustomDesign.order(created_at: :desc).limit(12)
+
+    @trending_custom_designs = CustomDesign
+      .joins(:hearts)
+      .group('custom_designs.id')
+      .order('count(hearts.id) DESC')
+      .where('hearts.created_at >= now()::date')
+      .limit(12)
+
+    @most_loved_custom_designs = CustomDesign.order(hearts_count: :desc).limit(12)
+
+    @most_loved_creators = Creator
+      .select('creators.*, sum(custom_designs.hearts_count) as hearts_count')
+      .joins(:custom_designs)
+      .group('creators.id')
+      .order('sum(custom_designs.hearts_count) DESC')
+      .limit(12)
   end
 end
