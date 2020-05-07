6d2cc0abe065a08035cb66dd3f5566b0179a205f
stephannv <stephann@protonmail.com>
2020-05-06 23:41:27 -0300

    Fix home pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    custom_design_scope = CustomDesign.published.order(created_at: :desc).limit(12)
+    custom_design_scope = CustomDesign.published.order(created_at: :desc)
 
     @pagy, @custom_designs = pagy(custom_design_scope)
   end
