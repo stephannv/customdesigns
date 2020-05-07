2437d117cbf047ce412f6f9b27a693cb11e51625
stephannv <stephann@protonmail.com>
2020-05-06 22:30:40 -0300

    Increase category name limit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_204235) do
+ActiveRecord::Schema.define(version: 2020_05_07_012918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_204235) do
   enable_extension "unaccent"
 
   create_table "categories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name", limit: 20, null: false
+    t.string "name", limit: 35, null: false
     t.citext "slug", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
